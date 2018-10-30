1fdff38ba46efd48b13f0095cfd0e0fdeb4db588
juanferca <juanferca@hotmail.com>
2018-10-30 10:41:30 +0100

    Integración login y custom dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,32 @@ import { APP_ROUTES } from './app.routes';
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
-import { FormsModule } from '@angular/forms';
+
+// Servicios
+
+import { ServiceModule } from './services/service.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    
-    
-    
-   
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { IncrementadorComponent } from "../component/incrementador/incrementador.component";
 import { ChartsModule } from 'ng2-charts';
 import { GraficadonaComponent } from "../component/graficadona/graficadona.component";
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -26,7 +27,8 @@ import { GraficadonaComponent } from "../component/graficadona/graficadona.compo
         ProgressComponent,
         Graficas1Component,
         IncrementadorComponent,
-        GraficadonaComponent
+        GraficadonaComponent,
+        AccountSettingsComponent
        
     ],
     exports: [
